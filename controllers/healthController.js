68a776bc46c03da7abc5cbcf5ce5f394316929cc
Sowmi2006 <sowmikapalla@gmail.com>
2026-07-08 10:23:16 +0530

    Remove academyService import from healthController

--- controllers/healthController.js
+++ controllers/healthController.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
-const academyService = require('../services/academyService');
 const { protect, adminOnly } = require('../middleware/authMiddleware');
 
 const startTime = Date.now();
