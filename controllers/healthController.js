38a4fff0e9e8382b58202e3626bb98cc8126a970
Sowmi2006 <sowmikapalla@gmail.com>
2026-07-08 10:22:49 +0530

    Import academyService in healthController

--- controllers/healthController.js
+++ controllers/healthController.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
+const academyService = require('../services/academyService');
 const { protect, adminOnly } = require('../middleware/authMiddleware');
 
 const startTime = Date.now();
